e57a0c1fd668eae92d6b2b70d230ee538adb0e50
Keagel <18720666+keagel@users.noreply.github.com>
2024-06-02 12:19:40 +0200

    initial commit

---
+++ src/routes/TextField.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    type Props = {
+        helperTextPosition: 'inline-left' | 'inline-right' | 'popup' | 'below';
+    };
+</script>
+
+{#snippet popupButton(classes: string = '')}
+	<button
+		type="button"
+		class="[&>*]:pointer-events-none {classes}"
+	>
+        Button
+	</button>
+{/snippet}
